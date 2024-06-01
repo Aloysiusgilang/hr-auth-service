f03698159700aec13757244c4fcffb6fd038dd3b
Aloysius Gilang <gilangpramdya84@gmail.com>
2024-06-01 16:32:51 +0700

    fix: get use instance

--- api/services/user.ts
+++ api/services/user.ts
@@ -3,7 +3,7 @@ import db from "../db";
 import { users } from "../schema";
 
 export const getUserByUserId = async (userId: string) => {
-  const user = await db
+  const [user] = await db
     .select()
     .from(users)
     .where(eq(users.id, userId))
